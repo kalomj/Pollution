44f3e2b61c219d577aff608c65d311b42536f6ad
Marc Kalo <marc.kalo@gmail.com>
2015-04-03 19:49:32 -0400

    more tests

--- packages/custom/pollution/public/routes/pollution.js
+++ packages/custom/pollution/public/routes/pollution.js
@@ -4,7 +4,12 @@ angular.module('mean.pollution').config(['$stateProvider',
     function($stateProvider) {
         $stateProvider.state('My Routes', {
             url: '/pollution/',
-            templateUrl: 'pollution/views/gmap/index.html'
+            templateUrl: 'pollution/views/gmap/myroutes.html'
         });
+
+      $stateProvider.state('mapIndex', {
+        url: '/pollution/',
+        templateUrl: 'pollution/views/gmap/index.html'
+      });
     }
 ]);
