50b28271d0b742d49a60818770ca6d682e7dd762
kalomj <marc.kalo@gmail.com>
2016-02-10 21:32:35 -0500

    index to support reduction processing

--- serverbuild/mongo_setup.js
+++ serverbuild/mongo_setup.js
@@ -3,3 +3,4 @@ db.triangles.createIndex( { valid_date : 1, valid_time : 1, parameter_name : 1  
 db.hourlydata.createIndex( { measurement_key: 1, valid_date : 1, valid_time : 1 } );
 db.hourlydata.createIndex( { valid_date : 1, valid_time : 1, parameter_name : 1 } );
 db.hourlydata.createIndex( { measurement_key : 1 } );
+db.hourlydata.createIndex( { measurement_key : 1, bounded : 1 } );
