f9ed1cb0f5653a0cc3b309575cba748c6b0c61d8
kalomj <marc.kalo@gmail.com>
2016-02-13 11:54:24 -0500

    location index

--- serverbuild/mongo_setup.js
+++ serverbuild/mongo_setup.js
@@ -2,5 +2,4 @@ db.triangles.createIndex( { valid_date : 1, valid_time : 1, parameter_name : 1, 
 db.triangles.createIndex( { valid_date : 1, valid_time : 1, parameter_name : 1  } );
 db.hourlydata.createIndex( { measurement_key: 1, valid_date : 1, valid_time : 1 } );
 db.hourlydata.createIndex( { valid_date : 1, valid_time : 1, parameter_name : 1 } );
-db.hourlydata.createIndex( { measurement_key : 1 } );
-db.hourlydata.createIndex( { measurement_key : 1, bounded : 1 } );
+db.locations.createIndex( { measurement_key : 1 } );
