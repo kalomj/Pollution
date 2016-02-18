7546097d09b65dda88fda1da355e6953f7294948
kalomj <marc.kalo@gmail.com>
2016-02-18 18:28:15 -0500

    dirty index on delaunay jobs

--- serverbuild/mongo_setup.js
+++ serverbuild/mongo_setup.js
@@ -6,3 +6,4 @@ db.hourlydata.createIndex( { measurement_key : 1, bounded : 1, hour_code : 1 } )
 db.locations.createIndex( { measurement_key : 1, hour_code : 1 } );
 db.loadedfiles.createIndex( { hour_code : 1 } );
 db.delaunayjobs.createIndex( { hour_code : 1, parameter_name : 1 } );
+db.delaunayjobs.createIndex( { dirty : 1 } );
