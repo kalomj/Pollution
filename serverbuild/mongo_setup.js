45f891bb712d0f1a89d854587f5efad63fcfe8ed
kalomj <marc.kalo@gmail.com>
2016-02-21 15:55:05 -0500

    job control document insert

--- serverbuild/mongo_setup.js
+++ serverbuild/mongo_setup.js
@@ -9,3 +9,4 @@ db.locations.createIndex( { measurement_key : 1 } );
 db.loadedfiles.createIndex( { hour_code : 1 } );
 db.delaunayjobs.createIndex( { hour_code : 1, parameter_name : 1 } );
 db.delaunayjobs.createIndex( { dirty : 1 } );
+db.jobcontrol.insert({ job_name: "reload", running : 0});
