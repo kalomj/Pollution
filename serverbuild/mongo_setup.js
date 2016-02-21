e9d0da5cece344039d312cf662b78aceacd08512
kalomj <marc.kalo@gmail.com>
2016-02-20 21:11:15 -0500

    no hour_code in locations collection

--- serverbuild/mongo_setup.js
+++ serverbuild/mongo_setup.js
@@ -4,7 +4,7 @@ db.hourlydata.createIndex( { measurement_key: 1, valid_date : 1, valid_time : 1 
 db.hourlydata.createIndex( { valid_date : 1, valid_time : 1, parameter_name : 1 } );
 db.hourlydata.createIndex( { measurement_key : 1, bounded : 1, hour_code : 1 } );
 db.hourlydata.createIndex( { measurement_key : 1, hour_code : 1 } );
-db.locations.createIndex( { measurement_key : 1, hour_code : 1 } );
+db.locations.createIndex( { measurement_key : 1 } );
 db.loadedfiles.createIndex( { hour_code : 1 } );
 db.delaunayjobs.createIndex( { hour_code : 1, parameter_name : 1 } );
 db.delaunayjobs.createIndex( { dirty : 1 } );
